8a50648045c775a80d4f1b8a0fc0a4df0c347886
victorsalaun <victor.salaun@gmail.com>
2018-03-29 21:42:27 +0200

    add hesperides dockerized app using aws beanstalk

---
+++ aws/hesperides_app.tf
@@ -0,0 +1,22 @@
+resource "aws_elastic_beanstalk_application" "hesperides-app" {
+  name = "hesperides-app"
+  description = "hesperides-app"
+}
+
+resource "aws_elastic_beanstalk_environment" "hesperides-app-dev-env" {
+  name = "hesperides-app-dev"
+  application = "${aws_elastic_beanstalk_application.hesperides-app.name}"
+  solution_stack_name = "${var.aws_beanstalk_docker_stackname}"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "InstanceType"
+    value = "t2.micro"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "IamInstanceProfile"
+    value = "aws-elasticbeanstalk-ec2-role"
+  }
+}
--- aws/variables.tf
+++ aws/variables.tf
@@ -5,3 +5,8 @@ variable "aws_secret_key" {}
 variable "aws_region" {
   default = "eu-west-3"
 }
+
+# https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts.platforms.html#concepts.platforms.docker
+variable "aws_beanstalk_docker_stackname" {
+  default = "64bit Amazon Linux 2017.09 v2.9.1 running Docker 17.12.0-ce"
+}
